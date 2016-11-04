859e5460d898d2b1a5a388a2e987893fe8ca256c
Mark Valentine <markolas@alumni.stanford.edu>
2016-11-04 09:56:04 -0600

    social media share

--- src/app/stories/story-detail/story-detail.component.ts
+++ src/app/stories/story-detail/story-detail.component.ts
@@ -15,6 +15,7 @@ export class StoryDetailComponent implements OnInit {
   key: string;
   isAuth: any;
   story: Observable<Story>;
+  storyObject: Story;
 
   constructor(
     private route: ActivatedRoute,
@@ -34,6 +35,9 @@ export class StoryDetailComponent implements OnInit {
       this.key = params['key'];
       this.isAuth = this.adminService.isAuth();
       this.story = this.storyService.getStory(this.key);
+      this.story.subscribe(x => {
+        this.storyObject = x;
+      });
     });
   }
 
@@ -48,6 +52,18 @@ export class StoryDetailComponent implements OnInit {
   goToCollection(key: string) {
     let link = ['/collections', this.key];
     this.router.navigate(link);
+  }
+
+  facebook() {
+    console.log(this.router.url);
+    FB.ui(
+      {
+        method: 'feed',
+        name: this.storyObject.title,
+        description: this.storyObject.description,
+        picture: this.storyObject.image_url,
+        link: 'https://markvalentine.github.io/zinnia-stories'+this.router.url
+      }, function(response){});
   }
 
 }
