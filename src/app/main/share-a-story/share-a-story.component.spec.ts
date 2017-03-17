392c89966f0bd240232456dd9a7defb280526707
Mark Valentine <markolas@alumni.stanford.edu>
2017-03-17 10:19:21 -0500

    related stories and share skeleton

---
+++ src/app/main/share-a-story/share-a-story.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ShareAStoryComponent } from './share-a-story.component';
+
+describe('Component: ShareAStory', () => {
+  it('should create an instance', () => {
+    let component = new ShareAStoryComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/main/share-a-story/share-a-story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share-a-story',
+  templateUrl: './share-a-story.component.html',
+  styleUrls: ['./share-a-story.component.css']
+})
+export class ShareAStoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
