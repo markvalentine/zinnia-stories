43c9ef851b945bfa6e7d2f98599a139817289bb8
Mark Valentine <markolas@alumni.stanford.edu>
2017-03-19 20:23:47 -0500

    gh-pages reset

---
+++ advisor-card/advisor-card.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AdvisorCardComponent } from './advisor-card.component';
+
+describe('Component: AdvisorCard', () => {
+  it('should create an instance', () => {
+    let component = new AdvisorCardComponent();
+    expect(component).toBeTruthy();
+  });
+});
