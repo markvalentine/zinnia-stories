e10a5af35111747024cfbb526932b05921b45222
Mark Valentine <markolas@alumni.stanford.edu>
2017-03-19 20:17:22 -0500

    i hope this helps

---
+++ person-card/person-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-card',
+  templateUrl: './person-card.component.html',
+  styleUrls: ['./person-card.component.css']
+})
+export class PersonCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
