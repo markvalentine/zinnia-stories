42241ab4c7c39002cc1225c0b88c2ca4c81dad4c
Mark Valentine <markolas@alumni.stanford.edu>
2017-04-27 14:30:18 -0400

    new about page donate button new patterns

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -10,12 +10,50 @@ declare let ga: Function;
 })
 export class AboutComponent implements OnInit {
 
+  opened = {
+    bg : {
+      height: "440px"
+    },
+    cover: {
+      height: "440px",
+      padding: "151px 0",
+      background: "rgba(31,45,62,0.20)"
+    },
+    text: {
+      display: "block"
+    },
+    triangle: {
+      transform: "rotate(180deg)"
+    }
+  }
+
+  closed = {
+    bg : {
+      height: "210px"
+    },
+    cover: {
+      height: "210px",
+      padding: "20px",
+      background: "rgba(31,45,62,0.60)"
+    },
+    text: {
+      display: "none"
+    },
+    triangle: {
+      transform: "none"
+    }
+  }
+
+  intergenerational = { bg: {}, cover: {}, text: {}, triangle: {}, opened: false, scroll: 0 }
+  loneliness =        { bg: {}, cover: {}, text: {}, triangle: {}, opened: false, scroll: 0 }
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
   ) { }
 
   ngOnInit() {
+    this.close_all();
     this.route.fragment.forEach((value: String) => {
       if (value == "partners") {
         document.getElementById('partners').scrollIntoView();
@@ -33,5 +71,117 @@ export class AboutComponent implements OnInit {
       transport: 'beacon'
     });
   }
+
+  intergenerational_clicked() {
+    if (this.intergenerational.opened) {
+      //close both
+      // this.close_all();
+      this.close_intergenerational();
+    } else {
+      //open and set other to not opened not closed state
+      this.open_intergenerational();
+      // this.standby_loneliness();
+    }
+  }
+
+  loneliness_clicked() {
+    if (this.loneliness.opened) {
+      //close both
+      // this.close_all();
+      this.close_loneliness();
+    } else {
+      //open and set other to not opened not closed state
+      this.open_loneliness();
+      // this.standby_intergenerational();
+    }
+  }
+
+  open_intergenerational() {
+    this.intergenerational.scroll = window.scrollY;
+    this.intergenerational.bg = this.opened.bg;
+    this.intergenerational.cover = this.opened.cover;
+    this.intergenerational.text = this.opened.text;
+    this.intergenerational.triangle = this.opened.triangle;
+    this.intergenerational.opened = true;
+  }
+
+  open_loneliness() {
+    this.loneliness.scroll = window.scrollY;
+    this.loneliness.bg = this.opened.bg;
+    this.loneliness.cover = this.opened.cover;
+    this.loneliness.text = this.opened.text;
+    this.loneliness.triangle = this.opened.triangle;
+    this.loneliness.opened = true;
+  }
+
+  close_all() {
+    this.close_intergenerational();
+    this.close_loneliness();
+  }
+
+  close_intergenerational() {
+    this.intergenerational.bg = this.closed.bg;
+    this.intergenerational.cover = this.closed.cover;
+    this.intergenerational.text = this.closed.text;
+    this.intergenerational.triangle = this.closed.triangle;
+    this.intergenerational.opened = false;
+
+    // console.log(window.scrollY, this.intergenerational.scroll);
+    // document.getElementById('why').scrollIntoView({block: "start", behavior: "smooth"});
+    this.scrollUp(this.intergenerational.scroll);
+    // this.animateScroll(this.intergenerational.scroll);
+  }
+
+  close_loneliness() {
+    this.loneliness.bg = this.closed.bg;
+    this.loneliness.cover = this.closed.cover;
+    this.loneliness.text = this.closed.text;
+    this.loneliness.triangle = this.closed.triangle;
+    this.loneliness.opened = false;
+
+    this.scrollUp(this.loneliness.scroll);
+
+    // document.getElementById('why').scrollIntoView({block: "start", behavior: "smooth"});
+  }
+
+  scrollUp(to: number) {
+    if (window.scrollY > to) {
+      window.scrollTo(window.scrollX, Math.max(to, document.getElementById('why').offsetTop));
+    }
+  }
+
+  animateScroll(to: number) {
+    var from = window.scrollY;
+
+    var diff = (from - to) / 60
+
+    if(from > to) {
+      var id = setInterval(frame, 5);
+    }
+
+    function frame() {
+          if (from <= to) {
+              clearInterval(id);
+          } else {
+              console.log("dome")
+              from -= diff;
+              window.scrollTo(window.scrollX, from);
+        }
+    }
+  }
+
+  // standby_intergenerational() {
+  //   this.intergenerational.bg = this.bg.standby;
+  //   this.intergenerational.cover = this.cover.standby;
+  //   this.intergenerational.text = this.text.standby;
+  //   this.intergenerational.opened = false;
+  // }
+
+  // standby_loneliness() {
+  //   this.loneliness.bg = this.bg.standby;
+  //   this.loneliness.cover = this.cover.standby;
+  //   this.loneliness.text = this.text.standby;
+  //   this.loneliness.opened = false;
+  // }
 
 }
